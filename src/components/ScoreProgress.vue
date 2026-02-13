a346aafee20ba13758fa7fa1b38cd1edde22b352
Stanislav <revasa@RevaSA.local>
2026-02-13 23:08:53 +0300

    Add levels

--- src/components/ScoreProgress.vue
+++ src/components/ScoreProgress.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="progress">
     <h4 class="progress-level">
-      <span>42/50</span>
-      <span>2</span>
+      <span>{{ store.currentScore }} / {{ store.level.value }}</span>
+      <span>{{ store.level.level + 1 }}</span>
     </h4>
     <div class="progress-container">
-      <div class="progress-value" style="width: 20%"></div>
+      <div class="progress-value" :style="{ width: progress + '%'}"></div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import { useScoreStore } from '@/stores/score'
+
+const store = useScoreStore()
+
+const progress = computed(() => {
+  console.log(store.currentScore, store.level.value)
+
+  return (100 * store.currentScore) / store.level.value
+})
+</script>
